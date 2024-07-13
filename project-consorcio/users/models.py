b807df8a210e28c856f635264f524edf226827b5
lucianafamular <lucianafamular@gmial.com>
2024-07-13 20:56:48 -0300

    Fix: se elimina modelo user

--- project-consorcio/users/models.py
+++ project-consorcio/users/models.py
@@ -1,15 +1,3 @@
 from django.db import models
 #from tipo_usuario.models import TipoUsuario
 #from permiso_usuario.models import PermisoUsuario
-
-class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-    #id_permiso = models.ForeignKey(PermisoUsuario, on_delete=models.CASCADE)
-    #id_tipoUsuario = models.ForeignKey(TipoUsuario, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.username
--- project-consorcio/users/urls.py
+++ project-consorcio/users/urls.py
@@ -3,10 +3,5 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='users'),
-    path('store/', views.store, name='store_user'),
-    path('update/<int:user_id>/', views.update, name='update_user'),
-    path('show/<int:user_id>/', views.show, name='show_user'),
-    path('destroy/<int:user_id>/', views.destroy, name='destroy_user'),
-
+    path('', views.index, name='users')
 ]
